e1074c571623eea2fdd121aeb45f9a513bb1a2c5
keelus <hugomoreda@hotmail.com>
2024-10-08 16:40:16 +0200

    Fixed face UV coordinates for the cube.

--- src/graphics/mesh.cpp
+++ src/graphics/mesh.cpp
@@ -26,23 +26,23 @@ namespace Hiruki {
 
 			mesh.faces = {
 				// Front face
-				Mesh::Face(3, 1, 4, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
-				Mesh::Face(1, 2, 4, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				Mesh::Face(3, 1, 4, 0xFF0000FF, TexCoord(0, 1), TexCoord(0, 0), TexCoord(1, 1)),
+				Mesh::Face(1, 2, 4, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 0), TexCoord(1, 1)),
 				// Right face
-				Mesh::Face(4, 2, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
-				Mesh::Face(2, 6, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				Mesh::Face(4, 2, 8, 0xFF0000FF, TexCoord(0, 1), TexCoord(0, 0), TexCoord(1, 1)),
+				Mesh::Face(2, 6, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 0), TexCoord(1, 1)),
 				// Back face
-				Mesh::Face(8, 6, 7, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
-				Mesh::Face(6, 5, 7, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				Mesh::Face(8, 6, 7, 0xFF0000FF, TexCoord(0, 1), TexCoord(0, 0), TexCoord(1, 1)),
+				Mesh::Face(6, 5, 7, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 0), TexCoord(1, 1)),
 				// Left face
-				Mesh::Face(7, 5, 3, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
-				Mesh::Face(5, 1, 3, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				Mesh::Face(7, 5, 3, 0xFF0000FF, TexCoord(0, 1), TexCoord(0, 0), TexCoord(1, 1)),
+				Mesh::Face(5, 1, 3, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 0), TexCoord(1, 1)),
 				// Top face
-				Mesh::Face(7, 3, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
-				Mesh::Face(3, 4, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				Mesh::Face(7, 3, 8, 0xFF0000FF, TexCoord(0, 1), TexCoord(0, 0), TexCoord(1, 1)),
+				Mesh::Face(3, 4, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 0), TexCoord(1, 1)),
 				// Bottom face
-				Mesh::Face(1, 5, 2, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
-				Mesh::Face(5, 6, 2, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				Mesh::Face(1, 5, 2, 0xFF0000FF, TexCoord(0, 1), TexCoord(0, 0), TexCoord(1, 1)),
+				Mesh::Face(5, 6, 2, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 0), TexCoord(1, 1)),
 			};
 			
 			return mesh;
