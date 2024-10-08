7b48339bf5791751c33b083d0a5e864a68373666
keelus <hugomoreda@hotmail.com>
2024-10-08 13:17:03 +0200

    Extended example with a texture.

--- examples/example0/main.cpp
+++ examples/example0/main.cpp
@@ -1,10 +1,19 @@
 #include "engine.hpp"
+#include "graphics/texture.hpp"
 #include "graphics/mesh.hpp"
+#include <iostream>
+#include <memory>
 
 int main(void) {
-	Hiruki::Engine engine(200, 200, 5, 144);
+	std::cout << "hello!" << std::endl;
+	Hiruki::Engine engine(320*4, 200*4, 1, 144);
+
+	std::shared_ptr<Hiruki::Graphics::Texture> texture = std::make_shared<Hiruki::Graphics::Texture>("tiles.png");
+	uint32_t color = texture->pickColor(0, 0);
+
 
 	Hiruki::Graphics::Mesh cube = Hiruki::Graphics::Mesh::defaultCube();
+	cube.setTexture(texture);
 
 	engine.addMesh(cube);
 
