820ea8785806a2813afc297b914327e4dd014698
keelus <hugomoreda@hotmail.com>
2024-10-08 11:27:20 +0200

    Completed example cube mesh. Moved Mesh::Face to public section.

--- src/graphics/mesh.cpp
+++ src/graphics/mesh.cpp
@@ -1,4 +1,5 @@
 #include "mesh.hpp"
+#include "math/vector3.hpp"
 
 namespace Hiruki {
 	namespace Graphics {
@@ -10,6 +11,40 @@ namespace Hiruki {
 
 		Mesh Mesh::defaultCube() {
 			Mesh mesh;
+			mesh.scale = Math::Vector3::one();
+
+			mesh.vertices = {
+				Math::Vector3(-1, -1, -1),
+				Math::Vector3(1, -1, -1),
+				Math::Vector3(-1, 1, -1),
+				Math::Vector3(1, 1, -1),
+				Math::Vector3(-1, -1, 1),
+				Math::Vector3(1, -1, 1),
+				Math::Vector3(-1, 1, 1),
+				Math::Vector3(1, 1, 1),
+			};
+
+			mesh.faces = {
+				// Front face
+				Mesh::Face(3, 1, 4, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
+				Mesh::Face(1, 2, 4, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				// Right face
+				Mesh::Face(4, 2, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
+				Mesh::Face(2, 6, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				// Back face
+				Mesh::Face(8, 6, 7, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
+				Mesh::Face(6, 5, 7, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				// Left face
+				Mesh::Face(7, 5, 3, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
+				Mesh::Face(5, 1, 3, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				// Top face
+				Mesh::Face(7, 3, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
+				Mesh::Face(3, 4, 8, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+				// Bottom face
+				Mesh::Face(1, 5, 2, 0xFF0000FF, TexCoord(0, 0), TexCoord(0, 1), TexCoord(1, 1)),
+				Mesh::Face(5, 6, 2, 0xFF0000FF, TexCoord(0, 0), TexCoord(1, 1), TexCoord(1, 0)),
+			};
+			
 			return mesh;
 		}
 	}
--- src/graphics/mesh.hpp
+++ src/graphics/mesh.hpp
@@ -10,24 +10,24 @@
 namespace Hiruki {
 	namespace Graphics {
 		class Mesh {
-			class Face {
-				public:
-					Face() {};
-					Face(int vertexIndex0, int vertexIndex1, int vertexIndex2, uint32_t color,
-						TexCoord vertexTexcoord0, TexCoord vertexTexcoord1, TexCoord vertexTexcoord2) 
-							: vertexIndex0(vertexIndex0), vertexIndex1(vertexIndex1), vertexIndex2(vertexIndex2), color(color),
-							vertexTexcoord0(vertexTexcoord0), vertexTexcoord1(vertexTexcoord1), vertexTexcoord2(vertexTexcoord2) {}
-
-					int vertexIndex0;
-					int vertexIndex1;
-					int vertexIndex2;
-					uint32_t color;
-					TexCoord vertexTexcoord0;
-					TexCoord vertexTexcoord1;
-					TexCoord vertexTexcoord2;
-			};
 			public:
-				Mesh() {};
+				class Face {
+					public:
+						Face() {};
+						Face(int vertexIndex0, int vertexIndex1, int vertexIndex2, uint32_t color,
+							TexCoord vertexTexcoord0, TexCoord vertexTexcoord1, TexCoord vertexTexcoord2) 
+								: vertexIndex0(vertexIndex0), vertexIndex1(vertexIndex1), vertexIndex2(vertexIndex2), color(color),
+								vertexTexcoord0(vertexTexcoord0), vertexTexcoord1(vertexTexcoord1), vertexTexcoord2(vertexTexcoord2) {}
+
+						int vertexIndex0;
+						int vertexIndex1;
+						int vertexIndex2;
+						uint32_t color;
+						TexCoord vertexTexcoord0;
+						TexCoord vertexTexcoord1;
+						TexCoord vertexTexcoord2;
+				};
+				Mesh() : scale(Math::Vector3::one()) {};
 
 				static Mesh loadFromFile(std::string filename);
 				static Mesh defaultCube();
